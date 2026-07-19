53badb4dc0ef8ca898c747d09a696afcbb7af93e
MiguelitoBFMV <miguelvaras1999@gmail.com>
2026-07-19 15:51:03 -0400

    feat: create MVS Tracker core and mount MAL Insights module

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import include, path
 
-
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("mal_data.urls")),
+    path("", include("core.urls")),
+    path("anime/", include("mal_data.urls")),
 ]
